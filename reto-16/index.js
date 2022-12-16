8e193ccf5b8dcde5d2935b75fa920e31995e668f
facindito <facinditto@gmail.com>
2022-12-16 17:46:24 -0300

    add: Reto-16

--- reto-16/index.js
+++ reto-16/index.js
@@ -1,12 +1,12 @@
 function fixLetter(letter) {
   return letter
-    .trim()
-    .replace(/\s+/g, " ")
-    .replace(/\s([,.?!])/g, "$1")
-    .replace(/([?])+/g, "$1")
-    .replace(/([.?!]\s[a-z])/g, (letter, word) => word.toUpperCase())
-    .replace(/^([A-z])/g, (letter, word) => word.toUpperCase())
+    .trim() //Eliminar espacios al inicio y al final
+    .replace(/\s+/g, " ") // Eliminar múltiples espacios en blanco y dejar sólo uno
+    .replace(/\s([,.?!])/g, "$1") // Eliminando espacio antes del signo
+    .replace(/([?])+/g, "$1") // Eliminar signo repetidos
+    .replace(/([.?!]\s[a-z])/g, (letter, word) => word.toUpperCase()) // Mayúscula después de un signo
+    .replace(/^([A-z])/g, (letter, word) => word.toUpperCase()) // Mayúscula al principio del string
     .replaceAll("santa", "Santa")
     .replaceAll("claus", "Claus")
-    .replace(/([A-z])$/g, (letter, word) => word + ".");
+    .replace(/([A-z])$/g, (letter, word) => word + "."); // Punto al final del string
 }
